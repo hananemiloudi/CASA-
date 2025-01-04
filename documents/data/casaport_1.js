c64597b572e41522c1665204e0ababd33ab28d40
hananemiloudi <hananemiloudi01@gmail.com>
2025-01-04 12:18:34 +0100

    Add files via upload

---
+++ documents/data/casaport_1.js
@@ -0,0 +1,1 @@
+var json_casaport_1 = {"type":"FeatureCollection","name":"casaport_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","NOM":"GARE CASA PORT","X":294554.9,"Y":335668.21,"Type":"ONCF"},"geometry":{"type":"Point","coordinates":[-7.6128144059533,33.59945511249731]}}]}
